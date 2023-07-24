b47d5f994bc442764a5a6ae8d970d8fc33ce8106
ushinnary <kartushin.alexander97@gmail.com>
2023-07-24 08:10:06 +0200

    set new colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,20 @@
-return {
-  {
-    "folke/tokyonight.nvim",
-    opts = {
-      transparent = false,
-      styles = {
-        sidebars = "transparent",
-        floats = "transparent",
-      },
-    },
-  },
-}
+return {{
+    -- "folke/tokyonight.nvim",
+    -- opts = {
+    --   transparent = false,
+    --   styles = {
+    --     sidebars = "transparent",
+    --     floats = "transparent",
+    --   },
+    -- },
+    "projekt0n/github-nvim-theme",
+    config = function()
+        require("github-theme").setup({})
+        -- check if linux system has dark mode enabled
+        if vim.fn.system('gsettings get org.gnome.desktop.interface gtk-theme'):match('dark') then
+            vim.cmd('colorscheme github_dark')
+        else
+            vim.cmd('colorscheme github_light')
+        end
+    end
+}}

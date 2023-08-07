ba835adf63ee26dfed4aaf9592ed5ac43f7412a2
Ushinnary <kartushin.alexander97@gmail.com>
2023-08-07 19:12:05 +0200

    fix ln nvim folder + added gnome 44+ dark mode

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,20 +1,25 @@
-return {{
-    -- "folke/tokyonight.nvim",
-    -- opts = {
-    --   transparent = false,
-    --   styles = {
-    --     sidebars = "transparent",
-    --     floats = "transparent",
-    --   },
-    -- },
-    "projekt0n/github-nvim-theme",
-    config = function()
-        require("github-theme").setup({})
-        -- check if linux system has dark mode enabled
-        if vim.fn.system('gsettings get org.gnome.desktop.interface gtk-theme'):match('dark') then
-            vim.cmd('colorscheme github_dark')
-        else
-            vim.cmd('colorscheme github_light')
-        end
-    end
-}}
+return {
+	{
+		-- "folke/tokyonight.nvim",
+		-- opts = {
+		--   transparent = false,
+		--   styles = {
+		--     sidebars = "transparent",
+		--     floats = "transparent",
+		--   },
+		-- },
+		"projekt0n/github-nvim-theme",
+		config = function()
+			require("github-theme").setup({})
+			-- check if linux system has dark mode enabled
+			if
+				vim.fn.system("gsettings get org.gnome.desktop.interface gtk-theme"):match("dark")
+				or vim.fn.system("gsettings get org.gnome.desktop.interface color-scheme"):match("dark")
+			then
+				vim.cmd("colorscheme github_dark")
+			else
+				vim.cmd("colorscheme github_light")
+			end
+		end,
+	},
+}

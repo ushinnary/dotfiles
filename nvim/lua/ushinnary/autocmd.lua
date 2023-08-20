ff639fda764a4b62dd4d5b3887cb42a1efd9ba30
Ushinnary <kartushin.alexander97@gmail.com>
2023-08-20 12:59:45 +0200

    Remove old packer config fully

--- nvim/lua/ushinnary/autocmd.lua
+++
@@ -1,8 +0,0 @@
-local cmd = vim.cmd
-local api = vim.api
-
-local fmtGroup = api.nvim_create_augroup("fmt", { clear = true })
-api.nvim_create_autocmd("BufWritePre", {
-	command = "undojoin | Neoformat",
-	group = fmtGroup,
-})
--- nvim/lua/ushinnary/init.lua
+++
@@ -1,7 +0,0 @@
-require("ushinnary.lazy")
-require("ushinnary.set")
-require("ushinnary.setup")
-require("ushinnary.lsp")
-require("ushinnary.autocmd")
-require("ushinnary.keymaps")
-require("ushinnary.dap")

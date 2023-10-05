defdc5483bb5319cc1153d58417023c4b04f783b
ushinnary <kartushin.alexander97@gmail.com>
2023-10-05 21:53:40 +0200

    Update scripts

--- scripts/set-config.sh
+++ scripts/set-config.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-ln -sd ../nvim ~/.config &&
-\ ln -s ../.zshrc ~/ &&
-\ flatpak --user override --filesystem=/home/$USER/.icons/:ro &&
-\ flatpak --user override --filesystem=/usr/share/icons/:ro
+ln -sd ../nvim ~/.config && \
+ ln -s ../.zshrc ~/

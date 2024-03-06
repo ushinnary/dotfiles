00a370035647e1a5782162784b4101db01be3f78
Ushinnary <kartushin.alexander97@gmail.com>
2024-03-06 20:37:45 +0100

    Added extras for debugging to nvim config
    Let me debug inside neovim plus updated terminal config

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -13,15 +13,22 @@ require("lazy").setup({
 		-- import any extras modules here
 		{ import = "lazyvim.plugins.extras.lang.typescript" },
 		{ import = "lazyvim.plugins.extras.lang.json" },
+		{ import = "lazyvim.plugins.extras.lang.rust" },
+		{ import = "lazyvim.plugins.extras.lang.tailwind" },
 		{ import = "lazyvim.plugins.extras.ui.mini-animate" },
 		-- import/override with your plugins
 		{ import = "lazyvim.plugins.extras.ui.edgy" },
+		{ import = "lazyvim.plugins.extras.ui.mini-animate" },
 
 		{ import = "lazyvim.plugins.extras.coding.copilot" },
-		{ import = "lazyvim.plugins.extras.lang.rust" },
-		{ import = "lazyvim.plugins.extras.lang.tailwind" },
-		{ import = "lazyvim.plugins.extras.linting.eslint" },
+
 		{ import = "lazyvim.plugins.extras.formatting.prettier" },
+
+		{ import = "lazyvim.plugins.extras.dap.core" },
+
+		{ import = "lazyvim.plugins.extras.editor.navic" },
+
+		{ import = "lazyvim.plugins.extras.test.core" },
 
 		{ import = "plugins" },
 	},

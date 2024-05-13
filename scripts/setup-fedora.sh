2bb58805839b6b1cd3c5634b8f87e282411981ee
Ushinnary <kartushin.alexander97@gmail.com>
2024-05-13 20:42:19 +0200

    impressive techs i discovered
    Using nushell as my main shell
    Using difftastic with lazygit
    Updated scripts

--- scripts/setup-fedora.sh
+++ scripts/setup-fedora.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-sudo dnf install openssl-devel cmake &&
+sudo dnf install openssl-devel cmake libxcb libX11-devel fontconfig-devel &&
 	sudo dnf group install "C Development Tools and Libraries" "Development Tools" &&
-	sudo dnf install zsh &&
-	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" &&
-	git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions &&
-	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
+	sudo dnf copr enable atim/lazygit -y &&
+	sudo dnf install lazygit
--- scripts/setup-rust.sh
+++ scripts/setup-rust.sh
@@ -11,4 +11,5 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh &&
 	cargo install sqlx-cli --no-default-features --features postgres &&
 	cargo install wasm-pack &&
 	cargo install bottom &&
-	cargo install --locked difftastic
+	cargo install --locked difftastic &&
+	cargo install nu

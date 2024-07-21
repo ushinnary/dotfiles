4a47808a30460ed65277a14150c66d25b791c0f3
Ushinnary <kartushin.alexander97@gmail.com>
2024-07-21 11:16:12 +0200

    Add fnm node manager

--- scripts/setup-rust.sh
+++ scripts/setup-rust.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh &&
-	cargo install cargo-update &&
-	cargo install ripgrep &&
-	cargo install fd-find &&
-	cargo install starship &&
-	cargo install exa &&
-	cargo install bat &&
-	cargo install tokei --features all &&
-	cargo install stylua &&
-	cargo install sqlx-cli --no-default-features --features postgres &&
-	cargo install wasm-pack &&
-	cargo install bottom &&
-	cargo install --locked difftastic &&
-	cargo install nu &&
-	cargo install --locked zellij
+  cargo install cargo-update &&
+  cargo install ripgrep &&
+  cargo install fd-find &&
+  cargo install starship &&
+  cargo install exa &&
+  cargo install bat &&
+  cargo install tokei --features all &&
+  cargo install stylua &&
+  cargo install sqlx-cli --no-default-features --features postgres &&
+  cargo install wasm-pack &&
+  cargo install bottom &&
+  cargo install --locked difftastic &&
+  cargo install nu &&
+  cargo install --locked zellij &&
+  cargo install fnm

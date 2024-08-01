fef8504828f77f344be8f471ccb4adeaad926bf1
Ushinnary <kartushin.alexander97@gmail.com>
2024-08-01 20:33:11 +0200

    Add wsl configuration

--- scripts/setup-ubuntu.sh
+++ scripts/setup-ubuntu.sh
@@ -2,4 +2,4 @@ sudo apt install -y \
   build-essential pkg-config autoconf bison clang cmake stow \
   libssl-dev libreadline-dev zlib1g-dev libyaml-dev libreadline-dev libncurses5-dev libffi-dev libgdbm-dev libjemalloc2 \
   libvips imagemagick libmagickwand-dev mupdf mupdf-tools unzip \
-  redis-tools sqlite3 libsqlite3-0 libmysqlclient-dev libpq-dev postgresql-client postgresql-client-common
+  redis-tools sqlite3 libsqlite3-0 libmysqlclient-dev libpq-dev postgresql-client postgresql-client-common python3-venv

c750f4551a4e1f54d93ef07a09a47f23a507360c
Ushinnary <kartushin.alexander97@gmail.com>
2024-11-17 11:30:49 +0100

    Added luarocks package to ubuntu install script

--- scripts/setup-ubuntu.sh
+++ scripts/setup-ubuntu.sh
@@ -2,4 +2,5 @@ sudo apt install -y \
   build-essential pkg-config autoconf bison clang cmake stow \
   libssl-dev libreadline-dev zlib1g-dev libyaml-dev libreadline-dev libncurses5-dev libffi-dev libgdbm-dev libjemalloc2 \
   libvips imagemagick libmagickwand-dev mupdf mupdf-tools unzip \
-  redis-tools sqlite3 libsqlite3-0 libmysqlclient-dev libpq-dev postgresql-client postgresql-client-common python3-venv
+  redis-tools sqlite3 libsqlite3-0 libmysqlclient-dev libpq-dev postgresql-client postgresql-client-common python3-venv \
+  liblua5.1-0-dev luarocks

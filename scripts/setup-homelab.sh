4171b7b578ed2efd044fa13cbd7656f9742f651a
Ushinnary <kartushin.alexander97@gmail.com>
2025-07-21 21:36:15 +0200

    Update setup-homelab.sh to install additional cockpit components

---
+++ scripts/setup-homelab.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo rpm-ostree install cockpit-system \
+  cockpit-ostree \
+  cockpit-podman \
+  cockpit-storaged \
+  cockpit-networkmanager \
+  cockpit-selinux \
+  cockpit-sosreport \
+  cockpit-files
+

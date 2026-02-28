92df90cbd232558044a1f9a6e7c78a284874df82
Ushinnary <kartushin.alexander97@gmail.com>
2026-02-28 16:51:09 +0100

    feat: add SSH authorized keys for user ushinnary

--- nix/modules/users.nix
+++ nix/modules/users.nix
@@ -14,6 +14,9 @@
       "wheel"
     ];
     shell = pkgs.nushell;
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKHRsmOjUoYncjrQJgnBTiUeRxkzEsuclK7TgiWxTRmq ushinnary@ryzo"
+    ];
   };
 
   users.groups.ushinnary = { };

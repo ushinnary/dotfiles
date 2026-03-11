bde0ede7e3fb8f0c6d917b98823dabd18808df78
Ushinnary <kartushin.alexander97@gmail.com>
2026-03-11 18:24:54 +0100

    fix: remove dark theme preferences from GTK configuration

--- nix/modules/DE/niri/bar.nix
+++ nix/modules/DE/niri/bar.nix
@@ -29,12 +29,9 @@ in
             name = "Adwaita";
             package = pkgs.adwaita-icon-theme;
           };
-          gtk3.extraConfig.gtk-application-prefer-dark-theme = 1;
-          gtk4.extraConfig.gtk-application-prefer-dark-theme = 1;
         };
 
         dconf.settings."org/gnome/desktop/interface" = {
-          color-scheme = "prefer-dark";
           gtk-theme = "Adwaita";
           icon-theme = "Adwaita";
         };

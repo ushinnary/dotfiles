95976b4a4f1bbbb80311b73b990fbe060807269e
Alexander Kartushin <ushinnary@MacBook-Pro-de-Alexander.local>
2026-08-08 11:29:35 +0200

    Adding config obj to applications.nix

--- nix/modules/apps/applications.nix
+++ nix/modules/apps/applications.nix
@@ -10,15 +10,16 @@ in
 {
   options.ushinnary.apps.davinciResolve = lib.mkEnableOption "DaVinci Resolve Studio";
 
-  environment.systemPackages =
-    with pkgs;
-    [
-      firefox
-    ]
-    ++ lib.optionals cfg.davinciResolve [
-      davinci-resolve-studio
-    ];
+  config = {
+    environment.systemPackages =
+      with pkgs;
+      [
+        firefox
+      ]
+      ++ lib.optionals cfg.davinciResolve [
+        davinci-resolve-studio
+      ];
 
-  nixpkgs.config.allowUnfree = true;
-
+    nixpkgs.config.allowUnfree = true;
+  };
 }
